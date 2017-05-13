1f44b286437517fe4d6bd63a49ebf881d7d80439
akameco <akameco.t@gmail.com>
2017-05-14 03:31:24 +0900

    Fix

--- html.js
+++ html.js
@@ -35,7 +35,7 @@ module.exports = class extends Component {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="https://akameco.github.io/blog/favicon.ico" />
           {head.title.toComponent()}
           {head.meta.toComponent()}
           <TypographyStyle typography={typography} />
