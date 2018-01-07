98aa4879a6b2d4eff3d8a510f2f72a45e186f5e1
akameco <akameco.t@gmail.com>
2018-01-07 22:24:58 +0900

    Add script for nippo

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+'use strict'
+const fs = require('fs')
+const path = require('path')
+const meow = require('meow')
+const shell = require('shelljs')
+const moment = require('moment')
+const ejs = require('ejs')
+
+const cli = meow(`
+  Usage:
+    yarn run add-page <input>
+`)
+
+const { input } = cli
+const title = input[0]
+const dateStr = moment().format('YYYY-MM-DD')
+const titleWithDate = `${dateStr}-${title}`
+const outputDirPath = `pages/${titleWithDate}`
+const outputPath = path.join(outputDirPath, 'index.md')
+
+shell.mkdir(outputDirPath)
+
+if (fs.existsSync(outputPath)) {
+  console.log(`${outputPath} is already exist`)
+  process.exit(0)
+}
+
+ejs.renderFile(
+  path.resolve(__dirname, './template/index.md.ejs'),
+  { title, date: dateStr, path: `/${titleWithDate}/` },
+  (err, output) => {
+    fs.writeFileSync(outputPath, output)
+  }
+)
---
+++ scripts/nippo.js
@@ -0,0 +1,25 @@
+'use strict'
+const fs = require('fs')
+const path = require('path')
+const shell = require('shelljs')
+const moment = require('moment')
+const ejs = require('ejs')
+
+const dateStr = moment().format('YYYY-MM-DD')
+const outputDirPath = `pages/${dateStr}`
+const outputPath = path.join(outputDirPath, 'index.md')
+
+shell.mkdir(outputDirPath)
+
+if (fs.existsSync(outputPath)) {
+  console.log(`${outputPath} is already exist`)
+  process.exit(0)
+}
+
+ejs.renderFile(
+  path.resolve(__dirname, './template/index.md.ejs'),
+  { title: dateStr, date: dateStr, path: `/${dateStr}/` },
+  (err, output) => {
+    fs.writeFileSync(outputPath, output)
+  }
+)
